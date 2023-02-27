eabd8e00cbc561ecde4393d74de4061737fa6517
Luka Stupar <stupar22luka@gmail.com>
2023-02-27 10:43:13 +0100

    Added description pdf

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from pyppeteer import launch
 
 
 async def main():
-    browser = await launch({'headless': False, 'executablePath': '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'})
+    browser = await launch({'headless': False, 'executablePath': '/usr/bin/brave-browser-stable', 'userDataDir': '/home/luka/.config/BraveSoftware/Brave-Browser/lokejsn'})
     page = await browser.newPage()
     # await page.goto('https://www.kickstarter.com/projects/jonathanmann/the-songs-of-adelaide-and-abullah')
+    await page.setViewport({'width': 1920, 'height': 1080})
     await page.goto('https://www.kickstarter.com/projects/213062534/greeting-from-earth-zgac-arts-capsule-for-et')
     a = input("")
 
