d5503091c8fd200a4e5d16286f59418405752d1e
Luka Stupar <stupar22luka@gmail.com>
2023-02-28 12:27:26 +0100

    Preprocessing data

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 import pandas as pd
-df = pd.read_csv('dataset.csv', index_col=0, encoding='unknown-8bit')
-df.hist(column='category')
+mainData = pd.read_csv('dataset.csv', index_col=False)
+firstScrapedData = pd.read_csv('DataLuka.csv', index_col=False)
+secondScrapedData = pd.read_csv('DataStefan.csv', index_col=False)
 
+scrapedData = [firstScrapedData, secondScrapedData]
+scrapedData = pd.concat(scrapedData)
+print()
+
+mainDataSubset = mainData[mainData['ID'].isin(scrapedData["ID"].tolist())]
+
+joinedData = pd.merge(mainDataSubset, scrapedData, how='inner', on='ID')
+print(joinedData.columns)
+print(joinedData.values)
